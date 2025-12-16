17aa5c46b9b02ea3716e74b4f525efa190fe3fc4
Austin Hauter <hautera@me.com>
2025-12-15 18:04:43 -0800

    Added ief policy upload functionality

--- internal/provider/client.go
+++ internal/provider/client.go
@@ -61,8 +61,11 @@ func (c *GraphClient) getToken(ctx context.Context) (string, error) {
 	return token.Token, nil
 }
 
-func (c *GraphClient) doGraph(ctx context.Context, method, url string, body any) (*http.Response, error) {
-
+func (c *GraphClient) doGraph(
+	ctx context.Context,
+	method, url string,
+	body any,
+) (*http.Response, error) {
 	var buf *bytes.Buffer
 	var payload string
 
@@ -107,6 +110,70 @@ func (c *GraphClient) doGraph(ctx context.Context, method, url string, body any)
 
 	req.Header.Set("Authorization", "Bearer "+token)
 	req.Header.Set("Content-Type", "application/json")
+
+	resp, err := c.client.Do(req)
+	if err != nil {
+		tflog.Error(ctx, "Graph API request failed", map[string]any{
+			"error": err.Error(),
+		})
+		return nil, err
+	}
+
+	bodyBytes, _ := io.ReadAll(resp.Body)
+	resp.Body = io.NopCloser(bytes.NewBuffer(bodyBytes))
+
+	tflog.Debug(ctx, "Graph API response", map[string]any{
+		"status": resp.Status,
+		"body":   string(bodyBytes),
+	})
+
+	return resp, nil
+}
+
+func (c *GraphClient) doGraphXML(
+	ctx context.Context,
+	method, url string,
+	body *string,
+) (*http.Response, error) {
+	var buf *bytes.Buffer
+	var payload string
+
+	if body != nil {
+		buf = bytes.NewBufferString(*body)
+		payload = *body
+	} else {
+		buf = &bytes.Buffer{}
+		payload = "<empty>"
+	}
+
+	tflog.Debug(ctx, "sending Graph request", map[string]any{
+		"method":  method,
+		"url":     url,
+		"payload": payload,
+	})
+
+	req, err := http.NewRequest(method, url, buf)
+	if err != nil {
+		tflog.Error(ctx, "failed to build Graph HTTP request", map[string]any{
+			"error": err.Error(),
+		})
+		return nil, err
+	}
+
+	token, err := c.getToken(ctx)
+	if err != nil {
+		tflog.Error(ctx, "Error getting token from credential!", map[string]any{
+			"error": err.Error(),
+		})
+		return nil, err
+	} else {
+		tflog.Debug(ctx, fmt.Sprintf("Token value: %s", token))
+	}
+
+	req.Header.Set("Authorization", "Bearer "+token)
+	//Yes this is literally the exact same method as the one above with this one line changed.
+	//Sue me
+	req.Header.Set("Content-Type", "application/xml")
 
 	resp, err := c.client.Do(req)
 	if err != nil {
